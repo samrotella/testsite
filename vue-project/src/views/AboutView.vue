221b6cddcd736c1d1fdb53f1097e58b6912d35e7
samrotella <48039441+samrotella@users.noreply.github.com>
2023-08-29 16:59:54 -0400

    implementation

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button v-on:click="conversion()">Buy Now</button>
   </div>
 </template>
+
+<script>
+// import owlsight from "https://owlsight.onrender.com/main.js";
+
+export default {
+  name: "About",
+  data() {
+    return {
+    };
+  },
+  methods: {
+    conversion() {
+      owlsight.conversions('SKU-123', 42.22);
+    },
+  },
+};
+</script>
 
 <style>
 @media (min-width: 1024px) {
